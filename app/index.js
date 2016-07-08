62d7632cf4fcea471f1749b481d69d97ee72d14f
Ramon van Raaij <ramon.van.raaij@gmail.com>
2016-07-08 11:05:27 +0200

    added material-ui

--- app/index.js
+++ app/index.js
@@ -6,6 +6,12 @@ import { syncHistoryWithStore } from 'react-router-redux';
 import routes from './routes';
 import configureStore from './store/configureStore';
 import './app.global.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 
 const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
